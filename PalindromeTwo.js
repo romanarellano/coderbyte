570c96236637546a9291b03d98adc700589d9b84
Roman Arellano <romanarellano@yahoo.com>
2014-11-02 11:00:22 -0500

    #

---
+++ PalindromeTwo.js
@@ -0,0 +1,19 @@
+
+function PalindromeTwo(str){
+
+
+	var together = str.replace(/[^a-zA-Z]/g,"");
+
+	var lower = together.toLowerCase().split("");
+	var string = lower.join("");
+    var rev = lower.reverse().join("");
+	if(string == rev )
+	return true;
+    return false;
+
+}
+
+console.log(PalindromeTwo("Noel - sees Leon"));
+
+console.log(PalindromeTwo("A war at Taraw!"));
+//console.log(PalindromeTwo("Anne, I vote more cars race Rome-to-Vienna"));
