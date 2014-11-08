811d10b0da80702439057aa115287b9fc9ae6f32
Roman Arellano <romanarellano@yahoo.com>
2014-11-07 22:07:38 -0600

    using object

---
+++ SimpleMode.js
@@ -0,0 +1,25 @@
+function SimpleMode(arr){
+var count =1;   
+var obj={};
+var index=arr.length;
+
+for(i=0;i<arr.length;i++){
+obj[arr[i]]=obj[arr[i]] || 0;
+obj[arr[i]]++; 
+}
+
+for(var j in obj){
+
+  if(obj[j]>=count && arr.indexOf(parseInt(j))<index){
+count=obj[j];
+number =j;
+index=arr.indexOf(parseInt(j));
+
+  }
+}
+if(count>1)
+return number;
+return -1;
+}
+
+console.log(SimpleMode([3,3,4,4,5,5,2,2,1]))
