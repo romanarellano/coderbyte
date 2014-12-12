678ff68b504e9da7c9f58cf4b1e04df7028227f0
Roman Arellano <romanarellano@yahoo.com>
2014-12-12 12:37:59 -0600

    4 array for loops and 1 object for loop

--- Medium-Difficulty/tripleDouble.js
+++ Medium-Difficulty/tripleDouble.js
@@ -9,40 +9,41 @@ arr.push(numOne,numTwo)
 var sum =0;
 var tot =3;
 var legit =[];
-
+var legit2=[];
 for(var i=0;i<arr.length;i++){
-   
+  legit=[];
    for(var j=0;j<arr[i].length;j++){
        var bench = arr[i][j];
     sum=0;
    for(var b=j;b<arr[i].length;b++){
    
 
-  if(arr[i][b]==bench){
+  if(arr[i][b]==bench && legit.indexOf(bench)<0 ){
      sum++;
 
      if(sum>=tot){
-      legit.push(arr[i][j])
+     legit.push(arr[i][j])
+     legit2.push(arr[i][j])
       break;
   }}
 else  {
-  
   break;
 }}
 }
 tot--;
-}
-sum=0;
-var numCount={};
-legit.sort();
-   for(n=0;n<legit.length;n++ ){
-   numCount[legit[n]] =  numCount[legit[n]] || 0;
- numCount[legit[n]]++;
+
 }
 
-for(var digit in numCount){
+var numCount={};
+
+   for(n=0;n<legit2.length;n++ ){
+   numCount[legit2[n]] =  numCount[legit2[n]] || 0;
+ numCount[legit2[n]]++;
+}
+
+for(digit in numCount){
   if(numCount[digit]==2)return 1;
 }
 return 0;
 }
-console.log(tripleDoble(46577555,855779))
+console.log(tripleDoble(4666677555,855576679))
